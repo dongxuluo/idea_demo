b41b1257837615cf2eb928c8a7857c9bcf5c41ef
Luo Dongxu <luodongxu2@huawei.com>
2025-03-26 21:09:30 +0800

    add calculator test

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+    @Test
+    public void shouldReturn2When1Plus1() {
+        Calculator calculator = new Calculator();
+        int actualResult = calculator.compute(1, 1, '+');
+        int expectedResult = 2;
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void shouldReturn0When1Sub1() {
+        Calculator calculator = new Calculator();
+        int actualResult = calculator.compute(1, 1, '0');
+        int expectedResult = 0;
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+}
